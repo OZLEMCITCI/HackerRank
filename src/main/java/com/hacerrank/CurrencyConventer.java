e46a1efc07aa6b1298a1c97be9c8b994b6eb2737
OZLEMCITCI <ozlmctc@gmail.com>
2021-09-10 23:48:32 -0500

    new Question

---
+++ src/main/java/com/hacerrank/CurrencyConventer.java
@@ -0,0 +1,29 @@
+package com.hacerrank;
+
+import java.text.FieldPosition;
+import java.text.NumberFormat;
+import java.text.ParsePosition;
+import java.util.Currency;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CurrencyConventer {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        NumberFormat us =NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india =NumberFormat.getCurrencyInstance(new Locale("en","IN"));
+        NumberFormat france =NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        NumberFormat china =NumberFormat.getCurrencyInstance(Locale.CHINA);
+
+
+
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+}
--- src/main/java/com/hacerrank/JavaDateAndTime.java
+++ src/main/java/com/hacerrank/JavaDateAndTime.java
@@ -4,14 +4,18 @@ import ch.qos.logback.classic.pattern.DateConverter;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.Scanner;
+import java.util.*;
 
 public class JavaDateAndTime {
     public static void main(String[] args) {
+        List<Integer> list=new ArrayList<>();
+
+
+        
 
 
     }
+
 
 
 
